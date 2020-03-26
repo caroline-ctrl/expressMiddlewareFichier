8f7d839bf3bc44dd753650971d11b2f921fb1a39
caroline-ctrl <duon.caroline@gmail.com>
2020-03-26 11:57:06 +0100

    page accueil.html ok

--- routes/accueil.js
+++ routes/accueil.js
@@ -1,10 +1,11 @@
 // ce dont j'ai besoin
 const express = require('express');
+const path = require('path');
 const route = express.Router();
 
 route.get('/', (req, res, next) => {
     console.log('accueil');
-    res.redirect('/users');
+    res.sendFile(path.join(__dirname, '../', 'views', 'accueil.html'));
 });
 
 module.exports = route;
