c7a94151f46c8443aad991e55f8bc5ed73049258
caroline-ctrl <duon.caroline@gmail.com>
2020-03-26 12:00:26 +0100

    page 404.html ok

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const express = require('express');
 const accueilRoute = require('./routes/accueil');
 const formulaireRoute = require('./routes/formulaire');
 const parser = require('body-parser');
+const path = require('path');
 
 const app = express();
 app.use(parser.urlencoded({extended: true}));
 
 app.use(accueilRoute);
 app.use(formulaireRoute);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3000);
