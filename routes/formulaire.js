67a3d80a7aac1b5d8ead69f7d093f94b150b077c
caroline-ctrl <duon.caroline@gmail.com>
2020-03-27 10:14:04 +0100

    mise en place de file system

--- routes/formulaire.js
+++ routes/formulaire.js
@@ -1,6 +1,7 @@
 // ce dont j'ai besoin
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const route = express.Router();
 
@@ -9,17 +10,30 @@ route.get('/users', (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'views', 'formulaireNom.html'));
 });
 
-route.get('/users-image', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'formulaireImage.html'));
-})
+route.get('/users-fichier', (req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'formulaireFichier.html'));
+});
 
 
 route.post('/user-create', (req, res) => {
     res.send(`
+    <link rel="stylesheet" href="/styles/main.css">
     <body>
     <h1>Votre nom est : ${req.body.name}</h1>
-    <a href="/">Revenir a la page d'accueil</a>
+    <button type="submit"><a href="/">Revenir a la page d'accueil</a></button>
     </body>`);
 });
+
+
+route.post('/user-message', (req, res) => {
+    const message = req.body.message;
+    console.log(message);
+    fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.redirect('/');
+    })
+})
+
+
 
 module.exports = route;
